5bfe8f027c0e57863824db92662c8a60f2a96bba
jt851113 <33995805+jt851113@users.noreply.github.com>
2023-02-16 17:54:52 +0800

    初始化spring boot project

---
+++ springboot-mall/src/test/java/com/eddieliao/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddieliao.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
